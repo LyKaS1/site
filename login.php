d242193fa0a2ec5694f26cba2c420deaae58904d
LyKaS1 <98958385+LyKaS1@users.noreply.github.com>
2024-12-02 14:27:43 +0300

    Update login.php

--- login.php
+++ login.php
@@ -1,36 +1,31 @@
 <?php
-// Подключаем библиотеку RedBeanPHP
 require "rb-mysql.php";
 
-// Подключаемся к БД
 R::setup( 'mysql:host=localhost;логин',
         'имя бд', 'пароль );
 
-// Проверка подключения к БД
 if(!R::testConnection()) die('No DB connection!');
 
 session_start(); // Создаем сессию для авторизации
-// Создаем переменную для сбора данных от пользователя по методу POST
 $data = $_POST;
 
-// Пользователь нажимает на кнопку "Авторизоваться" и код начинает выполняться
 if(isset($data['do_login'])) { 
 
- // Создаем массив для сбора ошибок
+
  $errors = array();
 
- // Проводим поиск пользователей в таблице users
+
  $user = R::findOne('users', 'login = ?', array($data['login']));
 
  if($user) {
 
- 	// Если логин существует, тогда проверяем пароль
+ 	
  	if(password_verify($data['password'], $user->password)) {
 
- 		// Все верно, пускаем пользователя
+ 		
  		$_SESSION['logged_user'] = $user;
  		
- 		// Редирект на главную страницу
+ 		
         header('Location: main.html');
 
  	} else {
